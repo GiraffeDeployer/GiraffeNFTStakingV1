66c6f98942b8fa4feb03e247457400d3c04c5ca4
GiraffeDeployer <slimpennies@gmail.com>
2024-06-27 21:25:15 -0400

    Update StakeRewards.tsx

--- components/StakeRewards.tsx
+++ components/StakeRewards.tsx
@@ -39,9 +39,9 @@ export const StakeRewards = () => {
     return (
         <div style={{ width: "100%", margin: "20px 0", display: "flex", flexDirection: "column" }}>
             {!isTokenBalanceLoading && (
-                <p>Wallet Balance: {toEther(BigInt(tokenBalance!.toString()))}</p>
+                <p>Giraffe Balance: {toEther(BigInt(tokenBalance!.toString()))}</p>
             )}
-            <h2 style={{ marginBottom: "20px"}}>Stake Rewards: {stakedInfo && toEther(BigInt(stakedInfo[1].toString()))}</h2>
+            <h2 style={{ marginBottom: "20px"}}>Giraffe Rewards: {stakedInfo && toEther(BigInt(stakedInfo[1].toString()))}</h2>
             <TransactionButton
                 transaction={() => (
                     prepareContractCall({
