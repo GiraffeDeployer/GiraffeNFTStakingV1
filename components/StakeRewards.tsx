1ca45a009fd312532917f6cda0c22d031b4db6d4
GiraffeDeployer <slimpennies@gmail.com>
2024-06-27 22:09:33 -0400

    Update StakeRewards.tsx

--- components/StakeRewards.tsx
+++ components/StakeRewards.tsx
@@ -4,10 +4,14 @@ import { REWARD_TOKEN_CONTRACT, STAKING_CONTRACT } from "../utils/contracts";
 import { prepareContractCall, toEther } from "thirdweb";
 import { balanceOf } from "thirdweb/extensions/erc721";
 
-const StakeRewards = () => {
+export const StakeRewards = () => {
     const account = useActiveAccount();
 
-    const { data: tokenBalance, isLoading: isTokenBalanceLoading, refetch: refetchTokenBalance } = useReadContract(
+    const {
+        data: tokenBalance,
+        isLoading: isTokenBalanceLoading,
+        refetch: refetchTokenBalance,
+    } = useReadContract(
         balanceOf,
         {
             contract: REWARD_TOKEN_CONTRACT,
@@ -15,33 +19,38 @@ const StakeRewards = () => {
         }
     );
     
-    const { data: stakedInfo, refetch: refetchStakedInfo } = useReadContract({
+    const {
+        data: stakedInfo,
+        refetch: refetchStakedInfo,
+    } = useReadContract({
         contract: STAKING_CONTRACT,
         method: "getStakeInfo",
         params: [account?.address || ""],
     });
 
     useEffect(() => {
-        refetchStakedInfo();
         const interval = setInterval(() => {
             refetchStakedInfo();
         }, 1000);
-        return () => clearInterval(interval);
-    }, []);
 
-    const formatBalance = (balance: string | number | undefined) => {
-        if (typeof balance !== 'undefined') {
-            return parseFloat(toEther(BigInt(balance.toString()))).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-        }
-        return "";
+        return () => {
+            clearInterval(interval);
+        };
+    }, [refetchStakedInfo]);
+
+    const formatBalance = (balance: number | string | bigint) => {
+        // Round balance to 2 decimal places
+        const roundedBalance = Number(balance).toFixed(2);
+        // Add commas to separate thousands
+        return roundedBalance.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     };
 
     return (
         <div style={{ width: "100%", margin: "20px 0", display: "flex", flexDirection: "column" }}>
-            {!isTokenBalanceLoading && (
-                <p>Giraffe Balance: {formatBalance(tokenBalance)}</p>
+            {!isTokenBalanceLoading && tokenBalance && (
+                <p>Giraffe Wallet Balance: {formatBalance(toEther(BigInt(tokenBalance.toString())))}</p>
             )}
-            <h2 style={{ marginBottom: "20px"}}>Giraffe Rewards: {stakedInfo && formatBalance(stakedInfo[1])}</h2>
+            <h2 style={{ marginBottom: "20px"}}>Giraffe Rewards Balance: {stakedInfo && formatBalance(toEther(BigInt(stakedInfo[1].toString())))}</h2>
             <TransactionButton
                 transaction={() => (
                     prepareContractCall({
@@ -50,7 +59,7 @@ const StakeRewards = () => {
                     })
                 )}
                 onTransactionConfirmed={() => {
-                    alert("Rewards claimed!")
+                    alert("Rewards claimed!");
                     refetchStakedInfo();
                     refetchTokenBalance();
                 }}
@@ -70,5 +79,3 @@ const StakeRewards = () => {
         </div>
     );
 };
-
-export default StakeRewards;
