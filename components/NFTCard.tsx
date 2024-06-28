bfab09d912fcf06202bf47a31c0129e5d21332ae
GiraffeDeployer <slimpennies@gmail.com>
2024-06-27 20:47:52 -0400

    Update NFTCard.tsx

--- components/NFTCard.tsx
+++ components/NFTCard.tsx
@@ -21,10 +21,10 @@ export const NFTCard = ({ nft, refetch, refecthStakedInfo }: OwnedNFTsProps) => 
                 client={client}
                 src={nft.metadata.image}
                 style={{
-                    borderRadius: "10px",
-                    marginBottom: "10px",
-                    height: "200px",
-                    width: "200px"
+                    borderRadius: "15px",
+                    marginBottom: "15px",
+                    height: "150px",
+                    width: "150px"
                 }}
             />
             <p style={{ margin: "0 10px 10px 10px"}}>{nft.metadata.name}</p>
