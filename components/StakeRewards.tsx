8fecb39e64117b8e2d997aad39b7569901e7e233
GiraffeDeployer <slimpennies@gmail.com>
2024-06-27 21:40:22 -0400

    Update StakeRewards.tsx

--- components/StakeRewards.tsx
+++ components/StakeRewards.tsx
@@ -1,10 +1,10 @@
+import React, { useEffect } from "react";
 import { TransactionButton, useActiveAccount, useReadContract } from "thirdweb/react";
 import { REWARD_TOKEN_CONTRACT, STAKING_CONTRACT } from "../utils/contracts";
 import { prepareContractCall, toEther } from "thirdweb";
-import { useEffect } from "react";
 import { balanceOf } from "thirdweb/extensions/erc721";
 
-export const StakeRewards = () => {
+const StakeRewards = () => {
     const account = useActiveAccount();
 
     const {
@@ -36,16 +36,24 @@ export const StakeRewards = () => {
         return () => clearInterval(interval);
     }, []);
 
+    const formatNumber = (number) => {
+        // Check if number is valid
+        if (isNaN(number)) return "";
+        
+        // Round to two decimal places and add commas
+        return Number(number).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    };
+
     return (
         <div style={{ width: "100%", margin: "20px 0", display: "flex", flexDirection: "column" }}>
             {!isTokenBalanceLoading && (
-                <p>Giraffe Balance: {toEther(BigInt(tokenBalance!.toString()))}</p>
+                <p>Giraffe Balance: {formatNumber(toEther(BigInt(tokenBalance!.toString())))}</p>
             )}
-            <h2 style={{ marginBottom: "20px"}}>Giraffe Rewards: {stakedInfo && toEther(BigInt(stakedInfo[1].toString()))}</h2>
+            <h2 style={{ marginBottom: "20px"}}>Giraffe Rewards: {stakedInfo && formatNumber(toEther(BigInt(stakedInfo[1].toString())))}</h2>
             <TransactionButton
                 transaction={() => (
                     prepareContractCall({
-                        contract:STAKING_CONTRACT,
+                        contract: STAKING_CONTRACT,
                         method: "claimRewards",
                     })
                 )}
@@ -64,7 +72,11 @@ export const StakeRewards = () => {
                     width: "100%",
                     fontSize: "12px"
                 }}
-            >Claim Rewards</TransactionButton>
+            >
+                Claim Rewards
+            </TransactionButton>
         </div>
-    )
+    );
 };
+
+export default StakeRewards;
